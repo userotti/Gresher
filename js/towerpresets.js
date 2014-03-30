5508ad9a70dc4bea42644b0187cca68530b0cced
Christian Frauenstein <cfrauenstein@gmail.com>
2014-03-30 20:33:16 +0200

    gamecore.js implementation, en tween movement on mouse click.
    next are the world and camera classes

---
+++ js/towerpresets.js
@@ -0,0 +1,84 @@
+BASICJELLY = {
+	
+	"character_class" : "jelly",
+	"mass" : 100,
+	"health" : 5,
+	"speed" : 0.2,
+ 	"shield": 0,
+ 	"shielrecharge": 0,
+ 	"range": 30,
+ 	"reload": 80,
+ 	"damage": 1,
+ 	"energy": 0,
+ 	"energyrecharge": 0,
+ 	"magneticcharge": 0,
+ 	"magneticrange": 0,
+
+ 	"bodybounce" : 0.08,
+ 	"bodyrotation_speed": 0,
+ 	
+
+ 	
+
+
+
+}
+
+BASICSTALAGMITE = {
+	
+	"character_class" : "stalagmite",
+	"mass" : 100,
+	"health" : 2,
+	"speed" : 1,
+ 	"shield": 0,
+ 	"shielrecharge": 0,
+ 	"range": 70,
+ 	"reload": 20,
+ 	"damage": 1,
+ 	"energy": 100,
+ 	"energyrecharge": 1,
+ 	"magneticcharge": 0,
+ 	"magneticrange": 0,
+
+ 	"bodybounce" : 0.0,
+ 	"bodyrotation_speed": 0.01,
+ 
+
+}
+
+
+
+BASICSTRUCT = {
+	
+	"character_class" : "stalagmite",
+	"mass" : 100,
+	"health" : 3,
+	"speed" : 1,
+ 	"shield": 0,
+ 	"shielrecharge": 0,
+ 	"range": 50,
+ 	"reload": 30,
+ 	"damage": 3,
+ 	"energy": 0,
+ 	"energyrecharge": 0,
+ 	"magneticcharge": 0,
+ 	"magneticrange": 0,
+
+ 	"bodybounce" : 0.00,
+ 	"bodyrotation_speed": 0.1,
+ 
+
+}
+
+ONSCREENRANDOM = function(){ 
+
+	return({
+	
+	"posx" : Math.random(),
+	"posy" : Math.random(),
+	"velx" : Math.random()-0.5,
+	"vely" : Math.random()-0.5,
+ 	"accx" : 0,
+ 	"accy" : 0, });
+ 	
+}
