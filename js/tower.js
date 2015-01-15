6978b97034c187e1a889f078430574628470aae5
Christian Frauenstein <cfrauenstein@gmail.com>
2015-01-15 23:30:09 +0200

    bigger dudes

--- js/tower.js
+++ js/tower.js
@@ -233,8 +233,8 @@ Tower = gamecore.DualPooled('Tower',
 	 		//console.log("texture", texture);
 	 		this.body.setTexture(texture);
 	    	
-	 		this.body.scale.x = 0.15;
-	 		this.body.scale.y = 0.15;
+	 		this.body.scale.x = 0.25;
+	 		this.body.scale.y = 0.25;
 			
 			this.body.position.x = -((texture.width*this.body.scale.x)/2);
 	 		this.body.position.y = -((texture.height*this.body.scale.y)/2);
